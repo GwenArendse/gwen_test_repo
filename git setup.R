64c05cd1a51c61d2da8fe6dc5986952564f322b7
GwenArendse <gwen.arendse@uct.ac.za>
2024-09-20 14:08:49 +0200

    test repo setup

---
+++ git setup.R
@@ -0,0 +1,9 @@
+#steps to install r, rstudio and git
+# re git, download git
+# go to rstudio > tools > git/svn and check that the correct path has been specified
+#open terminal in r and 
+# git config --global user.name 'Your name'
+# git config --global user.email 'Your email linked to github acc'
+# next, navigate to file > NEW PROJECT > VERSION CONTROL > GIT
+# and clone a repo
+# commit and push changes
